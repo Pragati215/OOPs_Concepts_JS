560d855651f5e681f9e2b894736331ea02032b16
Pragati215 <dhakanepragati4@gmail.com>
2020-12-10 21:46:13 +0530

    Refactor :Switch case used to Calculate total share for each comapany

--- StockManagement/StackManagementMain.js
+++ StockManagement/StackManagementMain.js
@@ -4,6 +4,32 @@ const stockManagementData = fs.readFileSync('./StockManagement.JSON');
 let data = JSON.parse(stockManagementData);
 const StockManagement = require('./StockManagementBusinesslogic');
 
-console.log(StockManagement.tcs());
-console.log(StockManagement.infosys());
-console.log(StockManagement.vipro());
+function stockManagement(input) {
+      switch (parseInt(input)) {
+            case 1:
+                  console.log(data.TCS);
+                  console.log(StockManagement.tcs());
+                  break;
+            case 2:
+                  console.log(data.Infosys);
+                  console.log(StockManagement.infosys());
+                  break;
+            case 3:
+                  console.log(data.Vipro);
+                  console.log(StockManagement.vipro());
+                  break;
+            case 4:
+                  console.log("Exit");
+                  break;
+            default:
+                  console.log("Wrong choice");
+                  break;
+      }
+}
+for (i = 1; i < 11; i++) {
+      let input = readlineSync.question("enter choice : 1.Tcs 2.Infosys 3.Vipro 4.exit:");
+      if (input == 4) {
+            return;
+      }
+      console.log(stockManagement(parseInt(input)));
+}
