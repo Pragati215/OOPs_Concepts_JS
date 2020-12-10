f279b97350f4bfd0d803acec0effb0e3b0b83df2
Pragati215 <dhakanepragati4@gmail.com>
2020-12-11 00:21:55 +0530

    Refactor :commits are added & refactored file contents

--- InventoryManagement/InventoryManagementBusinessLogic.js
+++ InventoryManagement/InventoryManagementBusinessLogic.js
@@ -1,16 +1,23 @@
+/*******************************************************************************************************
+ * @problem : Json objects of rice, pulses and wheat,  calculate total from price and weight
+ * @fileName : InventoryManagementBusinessLogic.js
+ * @Auther  : Pragati Dhakane
+ *******************************************************************************/
+
+ //class is defined & exported to Main logic
 class Grains {
-    constructor(name,weight,price) {
+    constructor(name, weight, price) {
         this.name = name;
         this.weight = weight;
         this.price = price;
-    
-    }
 
+    }
+    //method created to calculate total price of grains
     calculation() {
         let total = this.weight * this.price;
-        console.log("Total price for " + this.name + " is " +total);
+        console.log("Total price for " + this.name + " is " + total);
         return total;
     }
-    
+
 }
 module.exports = Grains;
