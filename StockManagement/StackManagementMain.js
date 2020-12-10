fd11ba25e17f5af94f899de5bf99db5aef30d1da
Pragati215 <dhakanepragati4@gmail.com>
2020-12-10 21:42:01 +0530

    Refactor :Calculated total share for each comapany

--- StockManagement/StackManagementMain.js
+++ StockManagement/StackManagementMain.js
@@ -1,5 +1,9 @@
 const fs = require("fs");
+let readlineSync = require('readline-Sync');
 const stockManagementData = fs.readFileSync('./StockManagement.JSON');
 let data = JSON.parse(stockManagementData);
 const StockManagement = require('./StockManagementBusinesslogic');
-console.log(data);
+
+console.log(StockManagement.tcs());
+console.log(StockManagement.infosys());
+console.log(StockManagement.vipro());
--- StockManagement/StockManagementBusinessLogic.js
+++ StockManagement/StockManagementBusinessLogic.js
@@ -1,13 +1,36 @@
+const fs = require("fs");
+const stockManagementData = fs.readFileSync('./StockManagement.JSON');
+let data = JSON.parse(stockManagementData);
+
 class StockManagement {
-    constructure(...params) {
-        this.shareName = params[0];
-        this.numberOfShare = params[1];
-        this.sharePrice = params[2];
+
+    constructor(shareName, numberOfShare, sharePrice) {
+        this.shareName = shareName;
+        this.numberOfShare = numberOfShare;
+        this.sharePrice = sharePrice;
     }
-    totalShare() {
-        let totalShare = this.numberOfShare * this.sharePrice;
-        console.log("Total share price for " + this.shareName + " is " +totalShare);
-        return totalShare;
+    tcs() {
+        let tcsTotal = 0;
+        data.TCS.forEach(element => {
+            tcsTotal = element.numberOfShare * element.sharePrice
+        });
+        return " TCS Total shares is :" + tcsTotal;
+    }
+
+    infosys() {
+        let infosysTotal = 0;
+        data.Infosys.forEach(element => {
+            infosysTotal = element.numberOfShare * element.sharePrice
+        });
+        return " Infosys Total shares is :" + infosysTotal;
+    }
+
+    vipro() {
+        let viproTotal = 0;
+        data.Vipro.forEach(element => {
+            viproTotal = element.numberOfShare * element.sharePrice
+        });
+        return " Vipro Total shares is :" + viproTotal;
     }
 }
-module.exports = StockManagement;
+module.exports = new StockManagement;
