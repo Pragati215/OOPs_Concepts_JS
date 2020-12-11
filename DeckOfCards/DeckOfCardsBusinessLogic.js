d06034a3bb4d9390baec91805dc2183ee3b8bd5e
Pragati215 <dhakanepragati4@gmail.com>
2020-12-11 15:59:16 +0530

    Refactor :Deck of cards main logic & Business logic

--- DeckOfCards/DeckOfCardsBusinessLogic.js
+++ DeckOfCards/DeckOfCardsBusinessLogic.js
@@ -23,6 +23,7 @@ class DeckOfCards {
         let shfulledDeck = new Array(4)
 
         for (let i = 0; i < shfulledDeck.length; i++) {
+           
             shfulledDeck[i] = new Array(9)
         }
 
@@ -30,11 +31,11 @@ class DeckOfCards {
         for (let j = 0; j < 9; j++) {
 
             for (let k = 0; k < 4; k++) {
-
+               
                 let rankindex = (Math.round(Math.random() * 10) % 13)
                 let suitindex = (Math.round(Math.random() * 10) % 4)
                 let card = this.suit[suitindex] + this.rank[rankindex]
-
+                
                 if (uniquecard.includes(card)) {
                     k--;
                 } else {
@@ -43,7 +44,7 @@ class DeckOfCards {
                 }
             }
         }
-        console.log(shfulledDeck);
+        console.log("cards distributed in 4 players -", shfulledDeck);
     }
 }
 module.exports = new DeckOfCards;  
