4816fe5323fc753697fa470be5768a58b6fdca94
Pragati215 <dhakanepragati4@gmail.com>
2020-12-11 15:38:29 +0530

    Refactor :Commits added Deck of cards main logic & Business logic

--- DeckOfCards/DeckOfCardsBusinessLogic.js
+++ DeckOfCards/DeckOfCardsBusinessLogic.js
@@ -1,26 +1,40 @@
 /*******************************************************************************************************
  * @problem :  write a program using array to distribute cards amongst 4 players
- * @fileName : DeckOfCards.js
+ * @fileName : DeckOfCardsBusinessLogic.js
  * @Auther  : Pragati Dhakane
  *******************************************************************************/
+
+//class is defined & exported to Main Logic 
+
 class DeckOfCards {
+
     constructor() {
         this.suit = ["Diamonds", "clubs", "Spades", "Hearts"]
         this.rank = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
     }
 
+ /* @description: card() is used to take DeckOfCards and shuffle them to distribute amongst players
+ * @purpose: it reads  array data from deck of cards 
+ */
+
     cards() {
+
         let uniquecard = new Array()
         let shfulledDeck = new Array(4)
+
         for (let i = 0; i < shfulledDeck.length; i++) {
             shfulledDeck[i] = new Array(9)
         }
 
+        //Distribute cards to 4 players
         for (let j = 0; j < 9; j++) {
+
             for (let k = 0; k < 4; k++) {
+
                 let rankindex = (Math.round(Math.random() * 10) % 13)
                 let suitindex = (Math.round(Math.random() * 10) % 4)
                 let card = this.suit[suitindex] + this.rank[rankindex]
+
                 if (uniquecard.includes(card)) {
                     k--;
                 } else {
@@ -31,5 +45,5 @@ class DeckOfCards {
         }
         console.log(shfulledDeck);
     }
-}   
- module.exports = new DeckOfCards;  
+}
+module.exports = new DeckOfCards;  
--- DeckOfCards/DeckOfCardsMain.js
+++ DeckOfCards/DeckOfCardsMain.js
@@ -1,2 +1,12 @@
+/*******************************************************************************************************
+ * @problem :  write a program using array to distribute cards amongst 4 players
+ * @fileName : DeckOfCardsMain.js
+ * @Auther  : Pragati Dhakane
+ *******************************************************************************/
+
+ // Business Logic file imported 
+
 let DeckOfCards = require('./DeckOfCardsBusinessLogic');
+
+//method calling using object creation
 DeckOfCards.cards();
